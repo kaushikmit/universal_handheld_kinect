917b9d2013013355bb98535a11cc781b2bcc9abd
Kaushik Ramachandran <kaushikmit94@gmail.com>
2017-04-30 19:17:43 +0530

    Initial Proto

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # universal_handheld_kinect
+
+#Follow Preinstall to Install dependencies
+
+#Steps to Connect
+
+Run app.py in seperate tab ( Port 5000 )
+
+Point cellphone to http://your-external-facing-ip:5000
+
+Rotate Mobile for live rotations !
---
+++ app.py
@@ -0,0 +1,73 @@
+from gevent import monkey
+monkey.patch_all()
+
+import time
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask.ext.socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, disconnect
+
+import redis
+
+p = redis.ConnectionPool()
+
+
+app = Flask(__name__, static_folder='static')
+app.debug = True
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+thread = None
+
+
+def background_thread():
+    """Dummy Thread"""
+
+
+@app.route('/')
+def index():
+    global thread
+    if thread is None:
+        thread = Thread(target=background_thread)
+        thread.start()
+    return render_template('index.html')
+
+
+@app.route('/receiver/<path:path>')
+def receiver(path):
+    return render_template('receiver/'+"receiver.html")
+
+@app.route('/app/<path:path>')
+def app_renderer(path):
+    return render_template('app/'+"heart.html")
+
+"""
+    Socket.io stream receiver
+"""
+
+@socketio.on('model::GYRO_STREAM', namespace='/model')
+def heart_gyro_receiver(message):
+    r = redis.Redis(connection_pool=p)
+    # Publish data on Redis PUB-SUB Channel
+    r.publish('model::GYRO_STREAM', message)
+    # Broadcast data to all connected clients
+    emit('model::GYRO_STREAM', message, broadcast=True)
+
+
+@socketio.on('model::ACCEL_STREAM', namespace='/model')
+def heart_gyro_receiver(message):
+    r = redis.Redis(connection_pool=p)
+    # Publish data on Redis PUB-SUB Channel
+    r.publish('model::ACCEL_STREAM', message)
+    # Broadcast data to all connected clients
+    emit('model::ACCEL_STREAM', message, broadcast=True)
+
+@socketio.on('model::COMMANDS', namespace='/model')
+def heart_commands_receiver(message):
+    r = redis.Redis(connection_pool=p)
+    # Publish data on Redis PUB-SUB Channel
+    r.publish('model::COMMANDS', message)
+    # Broadcast data to all connected clients
+    emit('model::COMMANDS', message, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host="0.0.0.0")
